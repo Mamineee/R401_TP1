616fefb0b0bc808b036ccba76ff9cdb518ed8232
Mohamed-Amin <mamine22123@gmail.com>
2024-01-26 09:15:39 +0100

    codage GetAll

---
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSConvertisseur.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+
+        public List<Devise> listDevise;
+
+        public DevisesController()
+        {
+            listDevise = new List<Devise>();
+            listDevise.Add(new Devise(1, "Dollar", 1.08));
+            listDevise.Add(new Devise(1, "Franc Suisse", 1.07));
+            listDevise.Add(new Devise(1, "Yen", 120));
+        }
+        // GET: api/<DeviseController>
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+
+            return listDevise;
+        }
+
+        // GET api/<DeviseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DeviseController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DeviseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DeviseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
