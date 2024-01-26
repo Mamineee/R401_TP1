bb9188f9fd7dae33b2f37df23c98fae4009eb880
Mohamed-Amin <mamine22123@gmail.com>
2024-01-26 08:39:56 +0100

    indexage

--- WSConvertisseur/Models/Devise.cs
+++ WSConvertisseur/Models/Devise.cs
@@ -1,6 +1,62 @@
-﻿namespace WSConvertisseur.Models
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace WSConvertisseur.Models
 {
     public class Devise
     {
+        int id;
+        string? nomDevise;
+        double taux;
+
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string? nomDevise, double taux)
+        { 
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string? NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return this.taux;
+            }
+
+            set
+            {
+                this.taux = value;
+            }
+        }
     }
 }
